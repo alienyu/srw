bad797ab7b927390d97e473003ed04108b9ea95d
alienyu <alienyu@foxmail.com>
2024-05-10 16:53:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+window.screenOrientation = "sensor_landscape",
+loadLib("libs/min/laya.core.min.js"),
+loadLib("libs/min/laya.particle.min.js"),
+loadLib("libs/min/laya.tiledmap.min.js"),
+loadLib("libs/min/laya.ui.min.js"),
+loadLib("libs/min/laya.physics.min.js"),
+loadLib("js/bundle.js");
